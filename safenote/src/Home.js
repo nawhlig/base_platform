b27d1ea58933cab55256c0c9d8795b62228fbd4f
HoonHoey <hoonhoey@gmail.com>
2020-11-09 16:01:55 +0900

    blink sentence, select box(layout)

--- safenote/src/Home.js
+++ safenote/src/Home.js
@@ -1,23 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Main.css';
 import './index.css';
 import 'antd/dist/antd.css';
 import 'antd/dist/antd.css';
 
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Select } from 'antd';
+import {Route, Link, NavLink, Switch } from 'react-router-dom';
 
 import test from './images/sliderImages/1918x954.gif';
 
 const { Header, Content, Footer } = Layout;
 
 function Home() {
+
+
+  const [text, setText] = React.useState("진정하세요");
+
+  React.useEffect(()=>{
+    const sentence = ["Calm Down","Beruhigen Sie sich","진정하세요"]
+    let cnt = 0
+    setInterval(function()
+    {
+      // Calm Down, Beruhigen Sie sich
+      setText(sentence[cnt]);
+      
+      if(cnt==2){
+        cnt=0
+      }
+      else {
+        cnt += 1;
+      }
+    },8000);
+  },[])
+
+  const { Option } = Select;
+
+function onChange(value) {
+  console.log(`selected ${value}`);
+}
+
+function onBlur() {
+  console.log('blur');
+}
+
+function onFocus() {
+  console.log('focus');
+}
+
+function onSearch(val) {
+  console.log('search:', val);
+}
+
   return (
     <>
       
         <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64, backgroundImage: "url(" + test + ")"}}>
             <div id='mainsentece'>
-            <p>진정하세요, Calm Down, Beruhigen Sie sich</p>
+            <p class="blink"><span>{text}</span></p>  
+            <Select
+            showSearch
+            style={{ width: 500 }}
+            placeholder="나라를 고르세요 / Select Your Contry"
+            optionFilterProp="children"
+            onChange={onChange}
+            onFocus={onFocus}
+            onBlur={onBlur}
+            onSearch={onSearch}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            <Option value="korea"><Link exact to="/korea">Korea</Link></Option>
+            <Option value="lucy">Lucy</Option>
+            <Option value="tom">Tom</Option>
+          </Select>
             </div>
         </Content>
        
--- safenote/src/Main.js
+++ safenote/src/Main.js
@@ -7,7 +7,7 @@ import 'antd/dist/antd.css';
 
 import {Route, Link, NavLink, Switch } from 'react-router-dom';
 import SubMenu from 'antd/lib/menu/SubMenu';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Select } from 'antd';
 
 import team from './images/team.jpg';
 import test from './images/sliderImages/1918x954.gif';
@@ -32,26 +32,30 @@ function Main() {
               <div className="logo" style={{float: "left", width: 150, padding: 'auto'}}><img src={logo} /></div>
               <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} style={{float:'right', backgroundColor: "black"}}>
                 <Menu.Item key="1"><Link exact to='/'>Home</Link></Menu.Item>
-                <SubMenu key="SubMenu" title="Emergency">
-                  <Menu.ItemGroup title="Asia / 아시아">
-                  <Menu.Item key="setting:1"><Link exact to="/korea">Korea / 한국</Link></Menu.Item>
-                  </Menu.ItemGroup>
-                  <Menu.ItemGroup title="Europe / 유럽">
-                  <Menu.Item key="setting:3">Sweden / 스웨덴</Menu.Item>
-                  <Menu.Item key="setting:4">U.K / 영국</Menu.Item>
-                  </Menu.ItemGroup>
-                  <Menu.ItemGroup title="Africa / 아프리카">
-                  <Menu.Item key="setting:3">Egypt / 이집트</Menu.Item>
-                  </Menu.ItemGroup>
-                  <Menu.ItemGroup title="America / 아메리카">
-                  <Menu.Item key="setting:3">U.S.A / 미국</Menu.Item>
-                  </Menu.ItemGroup>
-                </SubMenu>
-              </Menu>
+                  <SubMenu key="SubMenu" title="Emergency" placeholder="Search..">
+                    <Menu.ItemGroup title="Asia / 아시아">
+                    <Menu.Item key="setting:1"><Link exact to="/korea">Korea / 한국</Link></Menu.Item>
+                    </Menu.ItemGroup>
+                    <Menu.ItemGroup title="Europe / 유럽">
+                    <Menu.Item key="setting:3">Sweden / 스웨덴</Menu.Item>
+                    <Menu.Item key="setting:4">U.K / 영국</Menu.Item>
+                    </Menu.ItemGroup>
+                    <Menu.ItemGroup title="Africa / 아프리카">
+                    <Menu.Item key="setting:3">Egypt / 이집트</Menu.Item>
+                    </Menu.ItemGroup>
+                    <Menu.ItemGroup title="America / 아메리카">
+                    <Menu.Item key="setting:3">U.S.A / 미국</Menu.Item>
+                    </Menu.ItemGroup>
+                  </SubMenu>
+                </Menu>
             </Header>  
             <Switch>
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/korea" component={Korea}/>
+                <Route exact path="/sweden" component={Sweden}/>
+                <Route exact path="/egypt" component={Egypt}/>
+                <Route exact path="/usa" component={USA}/>
+                <Route exact path="/uk" component={UK}/>
             </Switch>
             <Footer id='footer'>SafeNote ©2020 Created by #Import</Footer>  
         </Layout>
@@ -60,5 +64,7 @@ function Main() {
     );
 
 }
+
+
 
 export default Main;
