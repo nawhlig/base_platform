486aaff2752102f7876e92a75a86448590982592
Yoonhyunyoung <taupo28@gmail.com>
2020-11-19 11:04:42 +0900

    지도 적용 및 안정화(20201119 by HY)

--- safenote/src/App.js
+++ safenote/src/App.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-
+import Map  from './Map';
+import { UserContext} from './userContext'
 import './App.css';
 import Main from './Main';
 
+
 function App() {
+
+  const [timedistance, setTimedistance] = useState({ totalTime:'',
+totalDistance: ''})
+
   return (
     <>
     <BrowserRouter>
+    <UserContext.Provider value={{timedistance:timedistance, setTimedistance:setTimedistance}}>
       <Main/>
+    </UserContext.Provider>
     </BrowserRouter>
     </>
   );
