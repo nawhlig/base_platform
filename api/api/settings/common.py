65c4934e11890eddaed7872c319ebc7eb89f8557
nawhlig <dksskd84@gmail.com>
2020-11-19 15:52:21 +0900

    (DRF 설정파일 추가) ALLOWED_HOSTS = ["127.0.0.1", "127.0.0.1:8000"]

--- api/api/settings/common.py
+++ api/api/settings/common.py
@@ -28,7 +28,7 @@ SECRET_KEY = private_django_secret_key.KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", "127.0.0.1:8000"]
 
 
 # Application definition
