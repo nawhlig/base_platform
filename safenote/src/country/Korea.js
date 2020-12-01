b371c09d647236eda51c81d576865933612afdbb
HoonHoey <hoonhoey@gmail.com>
2020-12-01 11:07:41 +0900

    put icon Korea

--- safenote/src/country/Korea.js
+++ safenote/src/country/Korea.js
@@ -6,7 +6,8 @@ import '../Main.css';
 
 import {Link} from 'react-router-dom';
 
-// import fire from './img/소방1.svg'
+import fire from '../img/소방1.svg'
+import map from '../img/지도1.svg'
 
 
 function Korea({history}){
@@ -14,10 +15,10 @@ function Korea({history}){
         <>
             <div className='countryheader'>Korea</div>
             <div className='sosbox'>
-                <a href='tel:01085586313'><button className='btn1'>Police</button></a>
-                <a href='tel:01085586313'><button className='btn2'>Fire Station</button></a>
-                <a href='tel:01085586313'><button className='btn3'>Ambulance</button></a>
-                <Link exact to='/koreamap'><button className='btn4'>Map</button></Link>
+                <a href='tel:01085586313'><button className='btn1'><img className="btnicon" src={fire}/><br/>Police</button></a>
+                <a href='tel:01085586313'><button className='btn2'><img className="btnicon" src={fire}/><br/>Fire Station</button></a>
+                <a href='tel:01085586313'><button className='btn3'><img className="btnicon" src={fire}/><br/>Ambulance</button></a>
+                <Link exact to='/koreamap'><button className='btn4'><img className="btnicon" src={fire}/><br/>Map</button></Link>
             </div>
         </>
     )
